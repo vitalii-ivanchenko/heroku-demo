d5ec7960bedbccee5a9b0b719be68f8d437807ef
Vitalii Ivanchenko <vitalik.ivanchenko@gmail.com>
2024-02-11 02:07:38 +0200

    first commit

---
+++ src/main/java/com/vivanchenko/heroku/HerokuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vivanchenko.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivanchenko/heroku/HerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivanchenko.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
